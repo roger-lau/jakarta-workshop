31579b86843c29b6baa760529539b0910a853f4f
Roger Lau <rlau@sonatype.com>
2020-03-05 00:09:24 +0800

    first upload

---
+++ target/tmp/jsp/org/apache/jsp/WEB_002dINF/content/orders_002dshow_jsp.java
@@ -0,0 +1,186 @@
+package org.apache.jsp.WEB_002dINF.content;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class orders_002dshow_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_s_property_value_nobody;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_s_property_value_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_s_property_value_nobody.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("<!DOCTYPE html>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<html lang=\"en\">\n");
+      out.write("<head>\n");
+      out.write("    <meta charset=\"utf-8\">\n");
+      out.write("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n");
+      out.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
+      out.write("    <title>Orders</title>\n");
+      out.write("    <link href=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${pageContext.request.contextPath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("/css/bootstrap.min.css\" rel=\"stylesheet\">\n");
+      out.write("    <link href=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${pageContext.request.contextPath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("/css/app.css\" rel=\"stylesheet\">\n");
+      out.write("\n");
+      out.write("    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->\n");
+      out.write("    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->\n");
+      out.write("    <!--[if lt IE 9]>\n");
+      out.write("    <script src=\"https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js\"></script>\n");
+      out.write("    <script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>\n");
+      out.write("    <![endif]-->\n");
+      out.write("</head>\n");
+      out.write("<body>\n");
+      out.write("<div class=\"container-fluid\">\n");
+      out.write("    <div class=\"row\">\n");
+      out.write("        <div class=\"col-md-12\">\n");
+      out.write("            <div class=\"page-header\">\n");
+      out.write("                <h1>Order ");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${id}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("</h1>\n");
+      out.write("            </div>\n");
+      out.write("            <table class=\"table table-striped\">\n");
+      out.write("                <tr>\n");
+      out.write("                    <td class=\"span3\">ID</td>\n");
+      out.write("                    <td class=\"span9\">");
+      if (_jspx_meth_s_property_0(_jspx_page_context))
+        return;
+      out.write("</td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td class=\"span3\">Client</td>\n");
+      out.write("                    <td class=\"span9\">");
+      if (_jspx_meth_s_property_1(_jspx_page_context))
+        return;
+      out.write("</td>\n");
+      out.write("                </tr>\n");
+      out.write("                <tr>\n");
+      out.write("                    <td class=\"span3\">Amount</td>\n");
+      out.write("                    <td class=\"span9\">");
+      if (_jspx_meth_s_property_2(_jspx_page_context))
+        return;
+      out.write("</td>\n");
+      out.write("                </tr>\n");
+      out.write("            </table>\n");
+      out.write("\t        <a href=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${pageContext.request.contextPath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("/orders\" class=\"btn btn-info\">\n");
+      out.write("\t\t        <span class=\"glyphicon glyphicon-arrow-left\"></span> Back to Orders\n");
+      out.write("\t        </a>\n");
+      out.write("        </div><!--/col-md-12--->\n");
+      out.write("    </div><!--/row-->\n");
+      out.write("</div><!--/container-->\n");
+      out.write("</body>\n");
+      out.write("</html>\n");
+      out.write("\t");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_s_property_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:property
+    org.apache.struts2.views.jsp.PropertyTag _jspx_th_s_property_0 = (org.apache.struts2.views.jsp.PropertyTag) _jspx_tagPool_s_property_value_nobody.get(org.apache.struts2.views.jsp.PropertyTag.class);
+    _jspx_th_s_property_0.setPageContext(_jspx_page_context);
+    _jspx_th_s_property_0.setParent(null);
+    _jspx_th_s_property_0.setValue("id");
+    int _jspx_eval_s_property_0 = _jspx_th_s_property_0.doStartTag();
+    if (_jspx_th_s_property_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_s_property_value_nobody.reuse(_jspx_th_s_property_0);
+      return true;
+    }
+    _jspx_tagPool_s_property_value_nobody.reuse(_jspx_th_s_property_0);
+    return false;
+  }
+
+  private boolean _jspx_meth_s_property_1(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:property
+    org.apache.struts2.views.jsp.PropertyTag _jspx_th_s_property_1 = (org.apache.struts2.views.jsp.PropertyTag) _jspx_tagPool_s_property_value_nobody.get(org.apache.struts2.views.jsp.PropertyTag.class);
+    _jspx_th_s_property_1.setPageContext(_jspx_page_context);
+    _jspx_th_s_property_1.setParent(null);
+    _jspx_th_s_property_1.setValue("clientName");
+    int _jspx_eval_s_property_1 = _jspx_th_s_property_1.doStartTag();
+    if (_jspx_th_s_property_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_s_property_value_nobody.reuse(_jspx_th_s_property_1);
+      return true;
+    }
+    _jspx_tagPool_s_property_value_nobody.reuse(_jspx_th_s_property_1);
+    return false;
+  }
+
+  private boolean _jspx_meth_s_property_2(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:property
+    org.apache.struts2.views.jsp.PropertyTag _jspx_th_s_property_2 = (org.apache.struts2.views.jsp.PropertyTag) _jspx_tagPool_s_property_value_nobody.get(org.apache.struts2.views.jsp.PropertyTag.class);
+    _jspx_th_s_property_2.setPageContext(_jspx_page_context);
+    _jspx_th_s_property_2.setParent(null);
+    _jspx_th_s_property_2.setValue("amount");
+    int _jspx_eval_s_property_2 = _jspx_th_s_property_2.doStartTag();
+    if (_jspx_th_s_property_2.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_s_property_value_nobody.reuse(_jspx_th_s_property_2);
+      return true;
+    }
+    _jspx_tagPool_s_property_value_nobody.reuse(_jspx_th_s_property_2);
+    return false;
+  }
+}
